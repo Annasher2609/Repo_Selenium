173b1076ae0ca43fc5319cc8de6dc52ecde224c5
Anna Shershneva <annashershneva@Annas-MacBook-Air.local>
2020-11-14 13:49:50 -0500

    Selenium project

---
+++ src/test/java/Class1/FirstClass.java
@@ -0,0 +1,16 @@
+package Class1;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstClass {
+    @Test
+    public void method1() {
+        System.out.println("hi");
+    }
+@Test
+    public void method2() {
+        System.out.println("hello");
+
+    }
+}
